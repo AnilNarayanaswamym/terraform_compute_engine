eef9607bcfb73917b0b5457f6045079f685a0d35
AnilNarayanaswamym <58822835+AnilNarayanaswamym@users.noreply.github.com>
2020-08-02 23:04:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,15 @@ resource "google_compute_instance" "default" {
 ]
   }
  }
+// adding firewalls
+// resource "google_compute_firewall" "http-server" {
+ // name    = "default-allow-http-terraform"
+ // network = "default"
+
+  //allow {
+    //protocol = "tcp"
+    //ports    = ["80"]
+  //}
 output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
